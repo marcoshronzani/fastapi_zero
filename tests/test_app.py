2cca70a9ff251a579b92fe62061e3b34e198261f
marcoshronzani <marcoshronzani@gmail.com>
2025-05-19 16:05:19 -0300

    Adição de schema de mensagem

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,4 +6,4 @@ from fastapi_zero.app import app
 def test_root_deve_retornar_ola_mundo():
     client = TestClient(app)
     response = client.get('/')
-    assert response.json() == {'message': 'Hello World'}
+    assert response.json() == {'message': 'Olá mundo!'}
